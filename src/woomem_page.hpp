c84f2c749717b78102977c25f0bca0bc5a722c0e
mr_cino <mr_cino@cinogama.net>
2026-06-16 01:01:30 +0800

    fix: align m_page_just_allocated to 8 bytes in PageHead

--- src/woomem_page.hpp
+++ src/woomem_page.hpp
@@ -12,7 +12,7 @@ namespace woomem
         // =================================================
         alignas(8) size_t       m_page_count_if_huge;
         alignas(8) PageHead*    m_next_page;
-        std::atomic_bool        m_page_just_allocated;
+        alignas(8) std::atomic_bool        m_page_just_allocated;
     };
     static_assert(sizeof(PageHead) == 24);
 }
